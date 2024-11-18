abf36d1337b6655d51ffaee9b82400644ccf46b7
0-ok-0 <azattnurr76@gmail.com>
2024-11-18 03:37:31 +0300

    Fix

--- script/nav_menu.js
+++ script/nav_menu.js
@@ -3,15 +3,17 @@ function prpr(){
     const сloseNavMenu = document.getElementById('nav_menu_phone')
 
     if(button.style.backgroundColor === 'rgb(151, 151, 151)'){
-        button.style.backgroundImage = 'url(../img/menu-icon.png)';
         button.style.backgroundColor = 'black';
         button.style.borderRadius = '0';
         сloseNavMenu.style.display = 'none';
+        document.getElementById('menu_icon').style.display = 'grid';
+        document.getElementById('close_nav_menu').style.display = 'none';
     } 
     else{
-        button.style.backgroundImage = 'url(../img/сlose_nav_menu.png)';
         button.style.backgroundColor = 'rgb(151, 151, 151)';
         button.style.borderRadius = '50%';
         сloseNavMenu.style.display = 'block';
+        document.getElementById('menu_icon').style.display = 'none';
+        document.getElementById('close_nav_menu').style.display = 'flex';
     }
 }
